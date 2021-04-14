3ab683246550284e2b93c148a5c47937e6980efe
casemiro <jv.casemiro@hotmail.com.br>
2021-04-14 20:27:20 -0300

    Utilizando SWR

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
+import useSWR from 'swr'
+
+const fetcher = (...args) => fetch(...args).then(res => res.json())
 
 const Index = () => {
+    const { data, error } = useSWR('/api/get-promo', fetcher)
     return (
         <div>
             <p className='mt-12 text-center'>
@@ -13,6 +17,12 @@ const Index = () => {
                     <a className="bg-blue-500 px-12 py-4 font-bold rounded-lg text-white shadow-lg hover:bg-blue-700">Dar opinião ou sugestão</a>
                 </Link>
             </div>
+            { !data && <p>Carregando...</p> }
+            { !error && data && data.showCoupon &&
+                <p className="my-12 text-center">
+                    {data.message}
+                </p>
+            }
         </div> 
     )
 }
