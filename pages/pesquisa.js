d17f525ddc2353f1398854152dd6dbbafacf029d
casemiro <jv.casemiro@hotmail.com.br>
2021-04-14 20:36:59 -0300

    Enviando dados via POST

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,6 +1,24 @@
 import React from 'react'
 
 const Pesquisa = () => {
+    const save = async () =>{
+        const form = {
+            Nome: 'aaa',
+            Email: 'bbb',
+            Whatsapp: 'ccc',
+            Sugestão: 'ddd'
+        }
+        
+        const response = await fetch('/api/save', {
+            method: 'POST',
+            body: JSON.stringify(form)
+        })
+        
+        const data = await response.json()
+
+        console.log(data);
+    }
+
     return (
         <div className='pt-6'>
             <h1 className='text-center font-bold my-4 text-2xl'>Criticas e Sugestões</h1>
@@ -20,6 +38,8 @@ const Pesquisa = () => {
 
                 <label className='font-bold'>Sua critica ou sugestão:</label>
                 <input className='bg-blue-100 p-5 shadow-md my-2 rounded block w-full'  type="text" />
+
+                <button onClick={save} className='bg-blue-500 px-12 py-4 font-bold rounded-lg text-white shadow-lg hover:bg-blue-700'>Salvar</button>
             </div>
         </div>
     )
