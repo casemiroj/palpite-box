8e7a6adb24aa604de93ed1e55bb3eb3977fc60fe
casemiro <jv.casemiro@hotmail.com.br>
2021-04-15 11:16:13 -0300

    Conectando a planilha

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,6 +1,29 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('1ZxXT3tK_4TVO_SXTOY4146G-m4hINNYRUzvEH9NfIaI')
+
 export default async(req, res) => {
-    res.end(JSON.stringify({
-        showCoupon: true,
-        message: 'Coupon message'
-    }))
+    try {
+        await doc.useServiceAccountAuth(credentials)
+        await doc.loadInfo()
+        
+        const sheet = doc.sheetsByIndex[2]
+        await sheet.loadCells('A2:B2')
+        
+        const mostrarPromocaoCell = sheet.getCell(1, 0)
+        const textoCell = sheet.getCell(1, 1)
+    
+        res.end(JSON.stringify({
+            showCoupon: mostrarPromocaoCell.value === 'Verdadeiro',
+            message: textoCell.value
+        }))
+    }
+
+    catch (err) {
+        res.end(JSON.stringify({
+            showCoupon: mostrarPromocaoCell.value === 'Falso',
+            message: ''
+        }))
+    }    
 }
--- pages/api/save.js
+++ pages/api/save.js
@@ -1,4 +1,28 @@
+import { GoogleSpreadsheet } from 'google-spreadsheet'
+import credentials from '../../credentials.json'
+
+const doc = new GoogleSpreadsheet('1ZxXT3tK_4TVO_SXTOY4146G-m4hINNYRUzvEH9NfIaI')
+
 export default async(req, res) => {
-    console.log(JSON.parse(req.body));
-    res.end(req.body)
+
+    try {
+        await doc.useServiceAccountAuth(credentials)
+        await doc.loadInfo()
+        const sheet = doc.sheetsByIndex[1]
+        const data = JSON.parse(req.body)
+        await sheet.addRow({
+            Nome: data.Nome,
+            Email: data.Email,
+            Whatsapp: data.Whatsapp,
+            Sugestao: data.Sugestao,
+            Cupom: '111111',
+            Promo: '10% OFF'
+        })
+        res.end(req.body)
+    }
+
+    catch (err) {
+        console.log(err)
+        res.end('error')
+    }
 }
